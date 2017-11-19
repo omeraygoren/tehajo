3d953df0858db5b4bb30147f17f3ddc34dc59787
omerAYGOREN01 <omer.aygoren@indbilisim.com.tr>
2017-11-19 14:23:33 +0300

    aaaa

---
+++ src/main/java/mongo/configs/AppConfigForTest.java
@@ -0,0 +1,9 @@
+package main.java.mongo.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"main.java.mongo"})
+public class AppConfigForTest {
+}
