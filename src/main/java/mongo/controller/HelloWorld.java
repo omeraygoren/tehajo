22509ff12b2bb2b47f1b065d12699a7aeae5f0e9
Ömer Aygören <omeraygoren@users.noreply.github.com>
2018-08-14 10:17:14 +0300

    Update HelloWorld.java

--- src/main/java/mongo/controller/HelloWorld.java
+++ src/main/java/mongo/controller/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
     public ModelAndView helloWorld() {
 
         String message = "<br><div style='text-align:center;'>"
-                + "<h3>********** En Son Burayi Degistirdim seni coni...... ************</h3>" +
+                + "<h3>********** merhaba...:)...... ************</h3>" +
                 "******** Merhaba **********</div><br><br>";
         return new ModelAndView("hello", "message", message);
     }
